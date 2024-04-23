1582e0ba11f4037acc9c043f87389b13e6dd1e79
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-04-23 16:38:43 +0900

    rename: express 경로 변경

--- newsstand/express/app.js
+++ newsstand/express/app.js
@@ -8,26 +8,26 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
 const data = JSON.parse(
-  fs.readFileSync("../newsstand/src/data/news.json", "utf8")
+  fs.readFileSync("../src/data/news.json", "utf8")
 );
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get("/api/channels", (req, res) => {
-  res.json(data.subscribe);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+app.get("/api/channels", (req, res) => {
+  res.json(data);
+});
 
-app.get('/api/users/channels', (req, res)=>{
+app.get("/api/users/channels", (req, res) => {
   res.send(data.subscribe);
 });
 
-app.post('/api/users/channels', (req, res) => {
+app.post("/api/users/channels", (req, res) => {
   const channel = {
-      id: req.body.id
+    id: req.body.id,
   };
   data.subscribe.push(channel);
   res.send(data.subscribe);
