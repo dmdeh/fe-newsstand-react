978d07f6630e8bf9f7bf97af744bbb33f88dbf9f
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-04-26 11:57:10 +0900

    styled: newsFlash style 추가

--- newsstand/src/js/NewsFlash/NewsFlash.jsx
+++ newsstand/src/js/NewsFlash/NewsFlash.jsx
@@ -4,8 +4,16 @@ export function NewsFlash() {
   return (
     <StyledDiv>
       <div className="newsFlash-content">
-        <div className="content-left"></div>
-        <div className="content-right"></div>
+        <StyledContent>
+          <b>연합뉴스</b>
+          <span>'尹·李 회담' 급물살…오늘 3차 실무회동서 날짜 정할듯</span>
+        </StyledContent>
+        <StyledContent>
+          <b>연합뉴스</b>
+          <span>
+            의대생들 "증원은 계약위반"…대학측 "민사 아닌 행정소송 대상"
+          </span>
+        </StyledContent>
       </div>
     </StyledDiv>
   );
@@ -20,11 +28,25 @@ const StyledDiv = styled.div`
     display: flex;
     justify-content: space-between;
   }
+`;
 
-  .content-left,
-  .content-right {
-    width: 49%;
-    height: 80px;
-    background-color: #ececec;
+const StyledContent = styled.div`
+  display: flex;
+  align-items: center;
+  width: 49%;
+  height: 80px;
+  background-color: #ececec;
+  
+  b {
+    padding: 10px;
+  }
+  
+  span {
+    font-size: 16px;
+    margin-left: 10px;
+    color: #666;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis; /* 말줄임*/
   }
 `;
